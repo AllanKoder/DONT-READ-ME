c0f6dca2db442384f0266328981ee9ff19b24323
Allan Kong <allankong176@gmail.com>
2024-11-08 10:02:46 +0000

    Improved deployment process with linking

--- compile_and_deploy.sh
+++ compile_and_deploy.sh
@@ -2,7 +2,7 @@
 # A script to help the deployment process of files
 
 # Base directory where the C++ source files are located
-BASE_DIR="."
+BASE_DIR="Routes"
 # Destination directory for CGI executables
 CGI_BIN="/usr/lib/cgi-bin"
 
@@ -12,15 +12,22 @@ find "$BASE_DIR" -name "*.cpp" | while read -r cpp_file; do
     dir=$(dirname "$cpp_file")
     # Get the base name of the .cpp file without extension
     base_name=$(basename "$cpp_file" .cpp)
-    
-    # Compile the C++ file
-    g++ "$cpp_file" -lmariadbcpp -lcgicc -o "$dir/$base_name.cgi"
-    
+
+    # Go to the directory containing the current .cpp file
+    cd "$dir" || { echo "Failed to change to directory: $dir"; exit 1; }
+
+    # Compile using make
+    make
+
     # Check if compilation was successful
     if [[ $? -eq 0 ]]; then
         # Move the compiled CGI executable to /usr/lib/cgi-bin/
-        mv "$dir/$base_name.cgi" "$CGI_BIN/"
-        echo "Successfully compiled and deployed: $base_name.cgi"
+        if [[ -f "$base_name.cgi" ]]; then
+            mv "$base_name.cgi" "$CGI_BIN/"
+            echo "Successfully compiled and deployed: $base_name.cgi"
+        else
+            echo "Compiled file not found: $base_name.cgi"
+        fi
     else
         echo "Failed to compile: $cpp_file"
     fi
